48f5589e8a03cf1795f7629e23d5bd1e1cf6bf4d
Elliot Holmes <133877996+eholm62@users.noreply.github.com>
2024-01-18 00:28:04 +0000

    modified:   aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,5 @@
-echo Hello World
-
+# $1 given in ~/.bashrc should be the path 
+# to the the base directory of the github repo    
+alias java="java -cp .:$1/StdLib"
+alias javac="javac -cp .:$1/StdLib" 
+alias runbashrc=". ~/.bashrc"
