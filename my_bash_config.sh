0425d16928687604b23838b4d86da2fc44e30254
eholm62 <133877996+eholm62@users.noreply.github.com>
2024-01-19 21:41:09 +0000

    made some small adjustments to my_bash_config.sh

--- my_bash_config.sh
+++ my_bash_config.sh
@@ -3,19 +3,52 @@
 # run from the base 
 # directory of the 
 # project by typing
-# . my_bash_config.sh
+# ". my_bash_config.sh"
 # do this every time  
 # you open a new terminal
+
 # after running once in 
 # the base directory you
 # can run from any dir
-# by typing config_bash
-# until you open a new 
-# terminal
+# by running "config_bash"
+# unless you open a new 
+# terminal in which case 
+# you would need to run 
+# ". my_bash_config.sh"
+# from the base directory again
 
-# $base_dir will be replaced
-# with the absolute path
+# if you "unalias -a" you 
+# will need to run 
+# ". $base_dir/my_bash_config.sh"
+# in order to get the 
+# "config_bash" alias back
+# along with all other
+# aliases
+
+# "$base_dir" will be replaced
+# by the absolute path
 # to the home directory
+
+# if you accidentally run
+# ". my_bash_config.sh"
+# somewhere other than 
+# the base directory of 
+# your project you can
+# open a new terminal
+# and run this file in the base
+# directory or you could
+# run "unset _already_run"
+# then run this file in 
+# the base directory
+
+# you may want to ignore 
+# most of this file 
+# and simply adjust then
+# lines beneath # aliases
+# but you also may benefit
+# from adjusting the behavior
+# of this program to your
+# liking
 
 # success does not 
 # necessarily mean that
