055a5c8aa4b3377f171418627849928ecea929a4
eholm62 <133877996+eholm62@users.noreply.github.com>
2024-01-22 22:28:15 +0000

    finished Takagi (in section_1_5/exercise_s3)

--- section_1_5/exercise_s3/Takagi.java
+++ section_1_5/exercise_s3/Takagi.java
@@ -12,17 +12,18 @@ class Takagi
 			sum += 1.0 / Math.pow(2, i) * sawtooth(Math.pow(2, i) * x);
 		return sum;
 	}
-	
+
 	public static void main(String[] args)
 	{
 		StdDraw.setPenRadius(0.0025);
-		
-		final int n = (int)Math.pow(2, Integer.parseInt(args[0]));
+
+		final int n = Integer.parseInt(args[0]);
+		final double bigN = Math.pow(2, n);
 
 		double[] prevPoint = new double[] { 0.0, 0.0 };
-		for (int i = 1; i <= n; i++)
+		for (int i = 1; i <= bigN; i++)
 		{
-			double x = (double)i / n;
+			double x = i / bigN;
 			double[] point = new double[] { x, takagi(n, x) };
 			StdDraw.line(prevPoint[0], prevPoint[1], point[0], point[1]);
 			prevPoint = point;
