29dc03df1f1887d8e5afeab4b13b30962b1a15a7
eholm62 <elliot.b.holmes@gmail.com>
2024-01-24 20:09:44 -0500

    updatyed my Turtle library

--- MyLibs/Turtle.java
+++ MyLibs/Turtle.java
@@ -6,7 +6,9 @@ class Turtle
 	double y;
 	double angle;
 	boolean isUp;
+	
 	Color color;
+	Draw screen;
 
 	public Turtle()
 	{
@@ -15,6 +17,8 @@ class Turtle
 		this.angle = 0.0;
 		this.isUp = false;
 		this.color = Color.BLACK;
+		
+		this.screen = new Draw();
 	}
 
 	public Turtle(double x, double y)
@@ -25,6 +29,8 @@ class Turtle
 		this.angle = 0.0;
 		this.isUp = false;
 		this.color = Color.BLACK;
+
+		this.screen = new Draw();
 	}
 
 	public Turtle(double x, double y, double angle)
@@ -35,16 +41,20 @@ class Turtle
 
 		this.isUp = false;
 		this.Color = Color.BLACK;
+
+		this.screen = new Draw();
 	}
 
 	public Turtle(double x, double y, double anglee, Color color)
 	{
 		this.x = x;
 		this.y = y;
-		this angle = angle;
+		this.angle = angle;
 		this.color = color;
 
 		this.isUp = isUp;
+
+		this.screen = new Draw();
 	}
 
 	public Turtle(double x, double y, double angle, Color color, boolean isUp)
@@ -54,6 +64,8 @@ class Turtle
 		this.angle = angle;
 		this.color = color;
 		this.isUp = isUp;
+
+		this.screen = new Draw();
 	}
 
 	public Turtle(double x, double y, double angle, Color color, boolean isUp)
@@ -63,7 +75,19 @@ class Turtle
 		this.angle = angle;
 		this.color = color;
 		this.isUp = isUp;
-		StdDraw.clear(clearColor);
+		
+		this.screen = new Draw();
+	}
+
+	public void clearScreen(Color color)
+	{
+		screen.clear(color);
+	}
+
+	public void setScreenScale(double minX, double maxX, double minY, double maxY)
+	{
+		screen.setXscale(minX, maxX);
+		screen.setYscale(minY, maxY);
 	}
 
 	public void clockwise(double degrees)
@@ -73,7 +97,7 @@ class Turtle
 
 	public void counterclockwise(double degrees)
 	{
-		andle - (angle + degrees) % 360.0;
+		angle = (angle + degrees) % 360.0;
 	}
 
 	public void penUp()
@@ -88,12 +112,31 @@ class Turtle
 
 	public void goTo(double x, double y)
 	{
-		StdDraw.line(x, y, this.x, this.y);
+		if (!penUp) screen.line(x, y, this.x, this.y);
 		this.x = x;
 		this.y = y;
 	}
 
 	public void forward(double distance)
 	{
-
+		double deltaX = Math.cos(Math.toRadians(this.angle)) * distance;
+		double deltaY = Math.sin(Math.toRadians(this.angle)) * distance;
+		if (!penUp) screen.line(this.x, this.y, this.x + deltaX, this.y + deltaY);
+		this.x += deltaX;
+		this.y += deltaY;
 	}
+
+	public static void main(String[] args)
+	{
+		Turtle turtle = new Turtle();
+		turtle.setScreenScale(-1.0, 1.0, -1.0, 1.0);
+
+		final int n = Integer.parseInt(args[0]);
+
+		for (int i = 1; i <= n; i++)
+		{
+			turtle.forward(0.25);
+			turtle.counterclockwise(360.0 / n);
+		}
+	}
+}
