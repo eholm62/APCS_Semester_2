f72bd57181b7d272766dc05eb504105d80b8f2ff
eholm62 <133877996+eholm62@users.noreply.github.com>
2024-01-26 21:35:37 +0000

    updated Turtle class and added fractals

--- MyLibs/Turtle.java
+++ MyLibs/Turtle.java
@@ -6,55 +6,58 @@ class Turtle
 	double y;
 	double angle;
 	boolean isUp;
-	
-	Color color;
-	Draw screen;
 
-	public Turtle()
+	Draw canvas;
+
+	public Turtle(Draw canvas)
 	{
 		this.x = 0.0;
 		this.y = 0.0;
 		this.angle = 0.0;
 		this.isUp = false;
-		this.color = Color.BLACK;
+		canvas.setPenColor(Color.BLACK);
+		canvas.setPenRadius(0.0025);
 		
-		this.screen = new Draw();
+		this.canvas = canvas;
 	}
 
-	public Turtle(double x, double y)
+	public Turtle(Draw canvas, double x, double y)
 	{
 		this.x = x;
 		this.y = y;
 		
 		this.angle = 0.0;
 		this.isUp = false;
-		this.color = Color.BLACK;
+		canvas.setPenColor(Color.BLACK);
+		canvas.setPenRadius(0.0025);
 
-		this.screen = new Draw();
+		this.canvas = canvas;
 	}
 
-	public Turtle(double x, double y, double angle)
+	public Turtle(Draw canvas, double x, double y, double angle)
 	{
 		this.x = x;
 		this.y = y;
 		this.angle = angle;
 
 		this.isUp = false;
-		this.Color = Color.BLACK;
+		canvas.setPenColor(Color.BLACK);
+		canvas.setPenRadius(0.0025);
 
-		this.screen = new Draw();
+		this.canvas = canvas;
 	}
 
-	public Turtle(double x, double y, double anglee, Color color)
+	public Turtle(Draw canvas, double x, double y, double anglee, Color color)
 	{
 		this.x = x;
 		this.y = y;
 		this.angle = angle;
-		this.color = color;
+		canvas.setPenColor(color);
 
 		this.isUp = isUp;
+		canvas.setPenRadius(0.0025);
 
-		this.screen = new Draw();
+		this.canvas = canvas;
 	}
 
 	public Turtle(double x, double y, double angle, Color color, boolean isUp)
@@ -62,32 +65,34 @@ class Turtle
 		this.x = x;
 		this.y = y;
 		this.angle = angle;
-		this.color = color;
+		canvas.setPenColor(color);
 		this.isUp = isUp;
 
-		this.screen = new Draw();
+		canvas.setPenRadius(0.0025);
+
+		this.canvas = canvas;
 	}
 
-	public Turtle(double x, double y, double angle, Color color, boolean isUp)
+	public Turtle(double x, double y, double angle, Color color, boolean isUp, double penRadius)
 	{
 		this.x = x;
 		this.y = y;
 		this.angle = angle;
-		this.color = color;
+		canvas.setPenColor(color);
 		this.isUp = isUp;
-		
-		this.screen = new Draw();
+		canvas.setPenRadius(penRadius);
+
+		this.canvas = canvas;
 	}
 
-	public void clearScreen(Color color)
+	public void setPenColor(Color color)
 	{
-		screen.clear(color);
+		canvas.setPenColor(color);
 	}
 
-	public void setScreenScale(double minX, double maxX, double minY, double maxY)
+	public void setPenRadius(double penRadius)
 	{
-		screen.setXscale(minX, maxX);
-		screen.setYscale(minY, maxY);
+		canvas.setPenRadius(penRadius);
 	}
 
 	public void clockwise(double degrees)
@@ -105,14 +110,14 @@ class Turtle
 		isUp = true;
 	}
 
-	public void penDown();
+	public void penDown()
 	{
 		isUp = false;
 	}
 
 	public void goTo(double x, double y)
 	{
-		if (!penUp) screen.line(x, y, this.x, this.y);
+		if (!isUp) canvas.line(x, y, this.x, this.y);
 		this.x = x;
 		this.y = y;
 	}
@@ -121,15 +126,25 @@ class Turtle
 	{
 		double deltaX = Math.cos(Math.toRadians(this.angle)) * distance;
 		double deltaY = Math.sin(Math.toRadians(this.angle)) * distance;
-		if (!penUp) screen.line(this.x, this.y, this.x + deltaX, this.y + deltaY);
+		if (!isUp) canvas.line(this.x, this.y, this.x + deltaX, this.y + deltaY);
 		this.x += deltaX;
 		this.y += deltaY;
 	}
 
+	public void pause(double seconds)
+	{
+		canvas.pause((int)(seconds * 1000));
+	}
+
 	public static void main(String[] args)
 	{
-		Turtle turtle = new Turtle();
-		turtle.setScreenScale(-1.0, 1.0, -1.0, 1.0);
+		Draw canvas = new Draw();
+		canvas.setXscale(-1.0, 1.0);
+		canvas.setYscale(-1.0, 1.0);
+		canvas.clear(Color.BLACK);
+		
+		Turtle turtle = new Turtle(canvas);
+		turtle.setPenColor(Color.WHITE);
 
 		final int n = Integer.parseInt(args[0]);
 
