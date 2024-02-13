6a54db02251e5a3fa89b5d93a5d25e9d7bf96098
eholm62 <133877996+eholm62@users.noreply.github.com>
2024-02-13 17:26:03 +0000

    finished 3.1.s6

--- section_3_1/exercise_s6/ScytaleDecoder.java
+++ section_3_1/exercise_s6/ScytaleDecoder.java
@@ -1,12 +1,32 @@
 class ScytaleDecoder
 {
+	static String removeHyphens(String s)
+	{
+		String newString = "";
+		for (int i = 0; i < s.length(); i++)
+		{
+			if (s.charAt(i) != '-')
+			{
+				newString += s.charAt(i);
+			}
+		}
+		return newString;
+	}
+	
 	public static void main(String[] args)
 	{
 		final int numFaces = Integer.parseInt(args[0]);
 
 		final String userInput = StdIn.readLine();
 
-		ArrayList<String> decodedLines = new ArrayList<String>();
-		for (int
+		String decoded = "";
+		for (int i = 0; userInput.charAt(i) != '-'; i++)
+		{
+			for (int j = i; j < userInput.length(); j += numFaces)
+			{
+				decoded += userInput.charAt(j);
+			}
+		}
+		StdOut.println(removeHyphens(decoded));
 	}
 }
