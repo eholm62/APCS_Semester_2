2365bb9ed3bbe0e696ea0970db878e36e748326f
Elliot Holmes <133877996+eholm62@users.noreply.github.com>
2024-02-26 15:02:17 +0000

    exercise 3.2.6 is incorrect

--- section_3_2/exercise_6/Location.java
+++ section_3_2/exercise_6/Location.java
@@ -3,7 +3,24 @@ class Location
 	private final double lattitude;
 	private final double longitude;
 
+	public Location(double lattitude, double longitude)
+	{
+		this.longitude = longitude;
+		this.lattitude = lattitude;
+	}
+
 	public double distanceTo(Location other)
 	{
+		double a = Math.sin(this.longitude) * Math.sin(other.longitude * other.longitude);
+		double b = Math.cos(this.longitude) * Math.cos(other.longitude) * Math.cos(this.lattitude - other.lattitude);
+		return 1.82 * 60 * Math.acos(a + b);
+	}
+
+	public static void main(String[] args)
+	{
+		Location london = new Location(51.51, -0.12);
+		Location paris = new Location(48.86, 2.35);
+		StdOut.println(london.distanceTo(paris));
+		StdOut.println(paris.distanceTo(london));
 	}
 }
